176b76f0397edcad44093f8709fd3080e925f036
Vincent Girard <vinccool96@gmail.com>
2023-07-23 16:30:33 -0400

    Useless

--- repo/gradle-settings-conventions/internal-gradle-setup/src/main/kotlin/PropertyValue.kt
+++
@@ -1,7 +0,0 @@
-internal sealed class PropertyValue(val value: String) {
-
-    class Configured(value: String) : PropertyValue(value)
-
-    class Overridden(value: String, val overridingValue: String) : PropertyValue(value)
-
-}
--- repo/gradle-settings-conventions/internal-gradle-setup/src/main/kotlin/SetupFile.kt
+++
@@ -1,4 +0,0 @@
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class SetupFile(val properties: Map<String, String>, val consentDetailsLink: String? = null)
